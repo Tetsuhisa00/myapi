e1ce34af9e9f1f748d1cf3ea68245416dc508d60
Tetsuhisa Suizu <tsuizu@c1r19s2.42tokyo.jp>
2023-04-30 22:44:09 -0400

    add postArticleHandler in main.go

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func main() {
     helloHandler := func(w http.ResponseWriter, req *http.Request) {
         io.WriteString(w, "Hello, world\n")
     }
+    
+    postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+        io.WriteString(w, "Posting Article...\n")
+    }
 
-    http.HandleFunc("/", helloHandler)
+
+    http.HandleFunc("/hello", helloHandler)
+    http.HandleFunc("/article", postArticleHandler)
 
     log.Println("server start at port 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))

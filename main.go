902c1ae8621ba0a181235c8613f62db122b38ae7
Tetsuhisa Suizu <tsuizu@c1r19s2.42tokyo.jp>
2023-04-30 23:00:45 -0400

    add articleDetailHandler in main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "io"
     "log"
     "net/http"
@@ -18,10 +19,17 @@ func main() {
     listArticleHandler := func(w http.ResponseWriter, req *http.Request) {
         io.WriteString(w, "Article List\n")
     }
+    
+    articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+        articleID := 1
+        resString := fmt.Sprintf("Article No.%d\n", articleID)
+        io.WriteString(w, resString)
+    }
 
     http.HandleFunc("/hello", helloHandler)
     http.HandleFunc("/article", postArticleHandler)
     http.HandleFunc("/article/list", listArticleHandler)
+    http.HandleFunc("/article/1", articleDetailHandler)
 
     log.Println("server start at port 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))

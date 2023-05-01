910378cb93b73bc4be4845748b58c88bc7cc6f28
Tetsuhisa Suizu <tsuizu@c1r19s2.42tokyo.jp>
2023-04-30 23:13:01 -0400

    add postNiceHandler in main.go

--- main.go
+++ main.go
@@ -25,11 +25,18 @@ func main() {
         resString := fmt.Sprintf("Article No.%d\n", articleID)
         io.WriteString(w, resString)
     }
+    
+    postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+        io.WriteString(w, "Posting Nice...\n")
+    }
+
 
     http.HandleFunc("/hello", helloHandler)
     http.HandleFunc("/article", postArticleHandler)
     http.HandleFunc("/article/list", listArticleHandler)
     http.HandleFunc("/article/1", articleDetailHandler)
+    http.HandleFunc("/article/nice",  postNiceHandler)
+
 
     log.Println("server start at port 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))

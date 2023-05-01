7bafae2ada0820740a358e5646f638d26fa8d212
Tetsuhisa Suizu <tsuizu@c1r19s2.42tokyo.jp>
2023-04-30 23:34:29 -0400

    move HandleFunctions to handlers/handlers

--- main.go
+++ main.go
@@ -1,48 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "io"
     "log"
     "net/http"
+    "github.com/Tetsuhisa00/myapi/handlers"
 )
 
 func main() {
-    helloHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, "Hello, world\n")
-    }
     
-    postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, "Posting Article...\n")
-    }
-
-    listArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, "Article List\n")
-    }
-    
-    articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
-        articleID := 1
-        resString := fmt.Sprintf("Article No.%d\n", articleID)
-        io.WriteString(w, resString)
-    }
-    
-    postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, "Posting Nice...\n")
-    }
-    
-    postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, "Posting Comment...\n")
-    }
 
 
-
-    http.HandleFunc("/hello", helloHandler)
-    http.HandleFunc("/article", postArticleHandler)
-    http.HandleFunc("/article/list", listArticleHandler)
-    http.HandleFunc("/article/1", articleDetailHandler)
-    http.HandleFunc("/article/nice",  postNiceHandler)
-    http.HandleFunc("/article/comment", postCommentHandler)
-
+    http.HandleFunc("/hello", handlers.HelloHandler)
+    http.HandleFunc("/article", handlers.PostArticleHandler)
+    http.HandleFunc("/article/list", handlers.ArticleListHandler)
+    http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+    http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+    http.HandleFunc("/comment", handlers.PostCommentHandler)
 
     log.Println("server start at port 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))

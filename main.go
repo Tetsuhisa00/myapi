634abd2dbc732a28a0cca7dcd75b99aff81aa8bb
Tetsuhisa Suizu <tsuizu@c1r19s2.42tokyo.jp>
2023-04-30 22:49:13 -0400

    add listArticleHandler in main.go

--- main.go
+++ main.go
@@ -15,9 +15,13 @@ func main() {
         io.WriteString(w, "Posting Article...\n")
     }
 
+    listArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+        io.WriteString(w, "Article List\n")
+    }
 
     http.HandleFunc("/hello", helloHandler)
     http.HandleFunc("/article", postArticleHandler)
+    http.HandleFunc("/article/list", listArticleHandler)
 
     log.Println("server start at port 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))

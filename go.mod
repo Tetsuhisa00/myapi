594eedb6950316bbbb0bfdec6c5bd3c95e704959
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-05-22 13:31:38 +0900

    Add gorilla/mux router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Tetsuhisa00/myapi
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -3,24 +3,23 @@ package main
 import (
     "log"
     "net/http"
+    "github.com/gorilla/mux"
     "github.com/Tetsuhisa00/myapi/handlers"
 )
 
 func main() {
+    
+    r := mux.NewRouter()
 
-    http.HandleFunc("/hello", handlers.HelloHandler)
-    http.HandleFunc("/article", handlers.PostArticleHandler)
-    http.HandleFunc("/article/list", handlers.ArticleListHandler)
-    http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-    http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-    http.HandleFunc("/comment", handlers.PostCommentHandler)
-
-
- // I'm done at 47 pages 4/30/2023
+    r.HandleFunc("/hello", handlers.HelloHandler)
+    r.HandleFunc("/article", handlers.PostArticleHandler)
+    r.HandleFunc("/article/list", handlers.ArticleListHandler)
+    r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+    r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+    r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 
     log.Println("server start at port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
-
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
 

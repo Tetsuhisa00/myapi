7419e52def1aa946651b29107fa8c1a558f49a2f
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-05-24 11:34:48 +0900

    add gorilla/ mux to each path except for /hello path

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
     r := mux.NewRouter()
 
     r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-    r.HandleFunc("/article", handlers.PostArticleHandler)
-    r.HandleFunc("/article/list", handlers.ArticleListHandler)
-    r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-    r.HandleFunc("/article/nice", handlers.PostNiceHandler)
-    r.HandleFunc("/comment", handlers.PostCommentHandler)
+    r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+    r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+    r.HandleFunc("/article/1", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+    r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+    r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
 
     log.Println("server start at port 8080")

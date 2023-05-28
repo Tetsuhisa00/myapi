de3fa077525a82e48dbb41e363201c0dc92f7d8c
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-05-28 18:40:15 +0900

    Path parameter(page66) was finshed.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,6 +7,8 @@ import (
    "fmt"
    "io"
    "net/http"
+   "strconv"
+   "github.com/gorilla/mux"
 )
 
 
@@ -33,7 +35,11 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 
 // /article/1 のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-    articleID := 1
+    articleID , err := strconv.Atoi(mux.Vars(req)["id"])
+    if err != nil {
+        http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+        return 
+    }
     resString := fmt.Sprintf("Article No.%d\n", articleID)
     io.WriteString(w, resString)
 }
